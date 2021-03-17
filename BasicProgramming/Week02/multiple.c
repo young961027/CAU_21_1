e4826bd01e6c57d77ebe0aa70f7ac179d99db90e
loopbackseal <young961027@gmail.com>
2021-03-17 18:34:58 +0900

    BP - W02 A1

---
+++ BasicProgramming/Week02/multiple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x;
+    int y;
+    int prod;
+
+    scanf("%d", &x);
+    scanf("%d", &y);
+    prod = x * y;
+    printf("곱셈의 결과= %d", prod);
+    return 0;
+}
